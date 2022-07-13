0f3924c2020e8cfb76cb49cd5c31835da9082d22
Fernweh99 <daniele.dimento@gmail.com>
2022-07-13 15:42:31 +0200

    add folder for bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,38 @@
+/*
+#Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+*/
+
+const App = new Vue({
+  name: 'ToDoList',
+  el: '#todo-list',
+  data: {
+    valueTask: "",
+    tasks: [
+      {
+        description: 'Fare la spesa',
+        isDone: true,
+      },
+      {
+        description: 'Fare l\'esercizio',
+        isDone: false,
+      },
+      {
+        description: 'Andare in banca',
+        isDone: true,
+      },
+    ],
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    addNewTask() {
+      if (this.valueTask.trim()) {
+        this.tasks.push({description: this.valueTask.trim(), isDone: false})
+        this.valueTask = "";
+      }
+    }
+  }
+})
